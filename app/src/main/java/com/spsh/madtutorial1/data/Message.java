7b5ba0017513b8d529feb4171e2697d9540c370a
IT21138386 <it21138386@my.sliit.lk>
2022-08-18 20:16:29 +0530

    Commit 1

---
+++ app/src/main/java/com/spsh/madtutorial1/data/Message.java
@@ -0,0 +1,13 @@
+package com.spsh.madtutorial1.data;
+//it is the best practise to store the data in a different fileAmbo
+public class Message {  //stores data
+    private String message = "";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
